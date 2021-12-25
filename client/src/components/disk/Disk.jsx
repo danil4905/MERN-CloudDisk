b9d01e2bc6def02b9e7825da6e72fd2a1ac0caec
Danil <conovalov.danil3441@gmail.com>
2021-12-25 15:22:32 +0400

    Add client upload and progress

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { getFiles } from "../../redux/actions/file";
+import { getFiles, uploadFile } from "../../redux/actions/file";
 import FileList from "./fileList/FileList";
 import './disk.scss'
 import Popup from "./Popup";
@@ -25,12 +25,21 @@ const Disk = () => {
         dispatch(setCurrentDir(backDirId))
     }
 
+    function fileUploadHandler(event) {
+        const files = [...event.target.files]
+        files.forEach(file => dispatch(uploadFile(file, currentDir)))
+    }
+
 
     return (
         <div className="disk">
             <div className="disk__btns">
                 <button className="disk__back" onClick={() => backClickHandler()}>Назад</button>
                 <button className="disk__create" onClick={() => showPopupHandler()}>Создать папку</button>
+                <div className="disk__upload">
+                    <label htmlFor="disk__upload-input" className="disk__upload-label">Загрузить файл</label>
+                    <input multiple={true} onChange={(event) => fileUploadHandler(event)} type="file" id="disk__upload-input" className="disk__upload-input" />
+                </div>
             </div>
             <FileList />
             <Popup />
