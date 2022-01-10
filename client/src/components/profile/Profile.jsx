48cb3c562d1f837561697f8e3fa1ea88cd23bcb8
Danil <conovalov.danil3441@gmail.com>
2022-01-10 21:34:15 +0400

    add back button

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useDispatch} from "react-redux";
 import {deleteAvatar, uploadAvatar} from "../../redux/actions/user";
+import {NavLink} from "react-router-dom";
 
 const Profile = () => {
     const dispatch = useDispatch()
@@ -12,6 +13,9 @@ const Profile = () => {
 
     return (
         <div>
+            <NavLink to={'/'} >
+                Назад
+            </NavLink>
             <button onClick={() => dispatch(deleteAvatar())}>Удалить аватар</button>
             <input accept="image/*" onChange={e => changeHandler(e)} type="file" placeholder="Загрузить аватар"/>
         </div>
